4a5416c9c03bec95400fbaf555c9a11d5ffb16e5
Alain Riou <alain.riou14000@yahoo.com>
2022-08-02 18:14:16 +0200

    replace random value by arg

--- torchaudio_augmentations/augmentations/delay.py
+++ torchaudio_augmentations/augmentations/delay.py
@@ -1,5 +1,3 @@
-import random
-import numpy as np
 import torch
 
 
@@ -7,26 +5,16 @@ class Delay(torch.nn.Module):
     def __init__(
         self,
         sample_rate,
-        volume_factor=0.5,
-        min_delay=200,
-        max_delay=500,
-        delay_interval=50,
+        volume_factor=0.5
     ):
         super().__init__()
         self.sample_rate = sample_rate
         self.volume_factor = volume_factor
-        self.min_delay = min_delay
-        self.max_delay = max_delay
-        self.delay_interval = delay_interval
 
     def calc_offset(self, ms):
         return int(ms * (self.sample_rate / 1000))
 
-    def forward(self, audio):
-        ms = random.choice(
-            np.arange(self.min_delay, self.max_delay, self.delay_interval)
-        )
-
+    def forward(self, audio, ms):
         offset = self.calc_offset(ms)
         beginning = torch.zeros(audio.shape[0], offset).to(audio.device)
         end = audio[:, :-offset]
--- torchaudio_augmentations/augmentations/noise.py
+++ torchaudio_augmentations/augmentations/noise.py
@@ -1,22 +1,14 @@
-import random
-import numpy as np
 import torch
 
 
 class Noise(torch.nn.Module):
-    def __init__(self, min_snr=0.0001, max_snr=0.01):
-        """
-        :param min_snr: Minimum signal-to-noise ratio
-        :param max_snr: Maximum signal-to-noise ratio
-        """
+    def __init__(self):
         super().__init__()
-        self.min_snr = min_snr
-        self.max_snr = max_snr
 
-    def forward(self, audio):
+    def forward(self, audio, snr):
         std = torch.std(audio)
-        noise_std = random.uniform(self.min_snr * std, self.max_snr * std)
+        noise_std = snr * std
 
-        noise = np.random.normal(0.0, noise_std, size=audio.shape).astype(np.float32)
+        noise = noise_std * torch.randn_like(audio)
 
         return audio + noise
